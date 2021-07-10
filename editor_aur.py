e34fcf13476b45059bfc372bce127c827008d973
M1R41 N1KK1 <66789407+M1R4I-N1KK1@users.noreply.github.com>
2021-07-10 10:49:40 -0300

    0.12 - distributed options management

--- editor_aur.py
+++ editor_aur.py
@@ -3,6 +3,7 @@ import final_process
 import hard_info
 from os import path
 import gi
+
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
@@ -25,17 +26,23 @@ mod = make = data
 class MainWindow(object):
 
     def __init__(self, *args, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.window_msg = builder.get_object('msg')
+
         self.entry_arquitetura_da_cpu = builder.get_object('entry_arquitetura_da_cpu')
-        self.auto_arquitetura_da_cpu = builder.get_object('auto_arquitetura_da_cpu')
+        self.arch_auto_button = builder.get_object('arch_auto_button')
+
         self.entry_core_da_cpu = builder.get_object('entry_core_da_cpu')
-        self.auto_core_da_cpu = builder.get_object('auto_core_da_cpu')
+        self.core_auto_button = builder.get_object('core_auto_button')
 
         self.aria2_manager = builder.get_object('aria2_manager')
         self.curl_manager = builder.get_object('curl_manager')
         self.axel_manager = builder.get_object('axel_manager')
 
         self.core = self.arch = self.manager_select = ''
+
+    def close_confirm_dialog_clicked(self, *args):
+        self.window_msg.hide()
 
     def main_window_destroy(self, *args):
         Gtk.main_quit()
@@ -52,23 +59,26 @@ class MainWindow(object):
         elif self.axel_manager.get_active():
             self.manager_select = 'axel'
 
-        if self.auto_arquitetura_da_cpu.get_active():
-            self.arch = hard_info.type_processor()
-        else:
-            if self.entry_arquitetura_da_cpu.get_text() == '':
+        if self.entry_arquitetura_da_cpu.get_text() == '':
+            if self.arch_auto_button.get_active():
+                self.arch = hard_info.type_processor()
+
+            else:
                 self.arch = 'generic'
-            else:
-                self.arch = str(self.entry_arquitetura_da_cpu.get_text())
 
-        if self.auto_core_da_cpu.get_active():
-            self.core = '$(($(nproc)+1)'
         else:
-            if self.entry_core_da_cpu.get_text() == '':
-                self.core = hard_info.proc_info()
-            else:
-                self.core = str(int(self.entry_core_da_cpu.get_text()) + 1)
+            self.arch = str(self.entry_arquitetura_da_cpu.get_text())
 
-        modification = mod.replace('CORECPU', self.core).replace('MANAGER', manager[self.manager_select][0])\
+        if self.entry_core_da_cpu.get_text() == '':
+            if self.core_auto_button.get_active():
+                self.core = '$(($(nproc)+1)'
+            else:
+                self.core = hard_info.proc_info()
+
+        else:
+            self.core = str(int(self.entry_core_da_cpu.get_text()) + 1)
+
+        modification = mod.replace('CORECPU', self.core).replace('MANAGER', manager[self.manager_select][0]) \
             .replace('ARCHCPU', self.arch)
 
         with open(resource_path('make_base'), 'wt') as base_make:
@@ -79,10 +89,11 @@ class MainWindow(object):
         with open(resource_path('make_base'), 'wt') as make_default:
             make_default.write(make)
 
+        self.window_msg.show_all()
+
 
 builder.connect_signals(MainWindow())
-window = builder.get_object('main_window')
-window.show_all()
+window = builder.get_object('main_window').show_all()
 
 if __name__ == '__main__':
     Gtk.main()
